76542df8919aa7d7116292d373fa11e747673f9b
rads <radhika.poddar@underscoretec.com>
2019-06-26 16:41:48 +0530

    first commit

---
+++ kotlin_toast_library/src/main/java/com/underscoretec/kotlin_toast_library/ToastClass.kt
@@ -0,0 +1,16 @@
+package com.underscoretec.kotlin_toast_library
+
+import android.content.Context
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ToastClass :AppCompatActivity(){
+
+    fun s(c:Context,msg : String){
+
+        val myToast = Toast.makeText(c,msg,Toast.LENGTH_SHORT)
+        myToast.show()
+
+    }
+
+}
